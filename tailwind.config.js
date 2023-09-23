97f98d84c7ca13f6401e058e866147211b3500f0
root <root@localhost>
2023-09-23 19:52:34 +0000

    Fixed front error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+      './storage/framework/views/*.php',
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
-
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+      extend: {},
     },
-
-    plugins: [forms],
-};
+    plugins: [
+      require("./plugin"),
+      require('tailwindcss-animated')
+    ],
+  }
+  
